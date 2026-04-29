730bc725b4174041fb6136ac072d437e4281f47c
preethamgowdap <prtmpreethu@gmail.com>
2026-04-29 13:39:27 +0530

    first commit

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-export type NoteCategory = 'personal' | 'work' | 'Birthday' | 'todo'  |'other';
-
-export type Note = {
-  id: string;
-  title: string;
-  body: string;
-  category: NoteCategory;
-};
